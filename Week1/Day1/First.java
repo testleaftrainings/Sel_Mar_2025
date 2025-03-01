79a30a4093555dc3c8e510a0e66553ffc414e10f
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-03-01 19:28:09 +0530

    Add files via upload

---
+++ Week1/Day1/First.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class First {
+
+	//type main and do ctrl+space
+	
+	public static void main(String[] args) {
+		
+		//type syso and do ctrl+space
+		
+		System.out.println("happy learning");
+		
+		//exection:ctrl+fn+F11
+	}
+}
---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+class LearnDatatypes {
+
+	public static void main(String[] args) {
+		
+		int regNo=456556;
+		
+		boolean isACar=true;
+		
+		float speed=1568.6766565f;
+		
+		char logo='S';
+		
+		String name="Skoda";
+		
+		 long phNO=1234567890L;
+		 
+		 double capacity=124.6765;
+		 
+		  String type="1254.656.6776.878";
+		  System.out.println(type);
+		  
+		  System.out.println(phNO);
+		 
+		 System.out.println("regNO is:"+regNo +" "+"name of the car is:"+name);
+		 
+
+	}
+	
+	
+	 
+
+}
